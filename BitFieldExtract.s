76efe5b5f2e9f26b4a1ea5ded6cf30471ccc1e14
josephcotter <jcotter@scu.edu>
2017-02-14 16:46:53 -0800

    Update BitFieldExtract.s

--- BitFieldExtract.s
+++ BitFieldExtract.s
@@ -8,11 +8,11 @@
 
         .global     UBFXtract
 UBFXtract:
-            LSR R0, R0, R1 //shifting right by lsb - unwanted bits to the right of bitfield are lost
-            RSB R2, R2, 32 //R3 = 32 - width
-            LSL R0, R0, R2 //shifting left by R2 - unwanted bits to left of bitfield are lost
-            LSR R0, R0, R2 //shifting right by R2 - returns extracted bits to correct position, with lsb of bitfield in bit position 0
-            BX  LR
+         LSR R0, R0, R1 //shifting right by lsb - unwanted bits to the right of bitfield are lost
+         RSB R2, R2, 32 //R3 = 32 - width
+         LSL R0, R0, R2 //shifting left by R2 - unwanted bits to left of bitfield are lost
+         LSR R0, R0, R2 //shifting right by R2 - returns extracted bits to correct position, with lsb of bitfield in bit position 0
+         BX  LR
 
         .end
 
